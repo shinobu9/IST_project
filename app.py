c00e2ed63a36a19ae56784951ff0e746ce93d925
mekan-yaldyrov <71642151+mekan-yaldyrov@users.noreply.github.com>
2022-12-12 20:42:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def start():
     ret = render_template("start.html")
     return ret
 
-@app.route("/museums/")
+@app.route("/museums")
 def museums():
     """Обрабатывает запрос к странице со списком музеев
        http://mipt-space-tis.ru:50XX/ """
@@ -18,7 +18,7 @@ def museums():
     ret = render_template("museums.html",museums_list=museums_list)
     return ret
 
-@app.route("/museums/<museum_id>")
+@app.route("/museum/<museum_id>")
 def museum(museum_id=None):
     """Обрабатывает запрос к странице конретного музея
        http://mipt-space-tis.ru:5000/author """
@@ -34,7 +34,7 @@ def museum(museum_id=None):
     "</pre>"
     return ret
 
-@app.route("/people/")
+@app.route("/people")
 def people():
     """Обрабатывает запрос к странице со списком авторов
        http://mipt-space-tis.ru:50XX/ """
@@ -42,7 +42,7 @@ def people():
     ret = render_template("people.html",authors_list=authors_list)
     return ret
 
-@app.route("/people/<people_id>")
+@app.route("/person/<people_id>")
 def person(people_id=None):
     """Обрабатывает запрос к странице конретного автора
        http://mipt-space-tis.ru:50XX/author """
