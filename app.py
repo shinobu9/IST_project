2cfba2b86188bbabf04b5e8226c5d71bff28fc4f
mekan-yaldyrov <71642151+mekan-yaldyrov@users.noreply.github.com>
2022-12-12 18:41:15 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template
+from db_lib import Books_Data
+
+app = Flask(__name__)
+bd = ArtsData()
+
+
+@app.route("/")
+def start():
+    ret = render_template("start.html")
+    return ret
+
+@app.route("/museums/")
+def museums():
+    """Обрабатывает запрос к странице со списком музеев
+       http://mipt-space-tis.ru:50XX/ """
+    museums_list = bd.get_museums()
+    ret = render_template("museums.html",museums_list=museums_list)
+    return ret
+
+@app.route("/museums/<museumId>")
+def museum(museumId=None):
+    """Обрабатывает запрос к странице конретного музея
+       http://mipt-space-tis.ru:5000/author """
+    full_info_museum = bd.get_full_info_museum(museumId)
+    museum_name = full_info_museum['museumName']
+    museum_date_found = full_info_museum['']
+    museum_city = full_info_museum['city']
+    museum_country = full_info_museum['country']
+    artworks = full_info_museum['artworks']
+    ret = render_template("museum.html",museum_name=museum_name,
+    museum_date_found=museum_date_found, museum_city=museum_city, museum_country=museum_country, artworks=artworks)
+    #ret ="<pre>" + str(museumName) +"\n" + str(countries) + "\n" + str(books) +
+    "</pre>"
+    return ret
+
+@app.route("/people/")
+def people():
+    """Обрабатывает запрос к странице со списком авторов
+       http://mipt-space-tis.ru:50XX/ """
+    authors_list = bd.get_people()
+    ret = render_template("people.html",authors_list=authors_list)
+    return ret
+
+@app.route("/people/<people_id>")
+def person(people_id=None):
+    """Обрабатывает запрос к странице конретного автора
+       http://mipt-space-tis.ru:50XX/author """
+
+    full_info_person = bd.get_full_info_person(people_id)
+    name = full_info_person['name']
+    birth = full_info_person['birthday']
+    death = full_info_person['deathDate']
+    countries = full_info_person['countries']
+    artworks = full_info_person['artworks']
+    ret = render_template("person.html",name=name,
+birth=birth, death=death, countries=countries, artworks=artworks)
+    #ret ="<pre>" + str(author_name) +"\n" + str(countries) + "\n" + str(books) +
+    "</pre>"
+    return ret
+
+@app.route("/artworks/")
+def artworks():
+    """Обрабатывает запрос к странице со списком авторов
+       http://mipt-space-tis.ru:50XX/ """
+    full_info_artworks = bd.get_full_info_artworks()
+    artwork_list = []
+
+    for i in range(len(full_info_artworks)):
+        d = {}
+        d['name'] = full_info_artworks[i][0]
+        d['type'] = full_info_artworks[i][1]
+        d['author'] = full_info_artworks[i][2]
+        d['date_created'] = full_info_artworks[i][3]
+        d['museum'] = full_info_artworks[i][4]
+        artwork_list.append(d)
+    ret = render_template("artworks.html",artwork_list=artwork_list)
+    return ret
+
+app.run(host='37.46.128.78',port=5023)
